ad23859bf493c5a031919f646430fc58b524f875
Cgana <patiencebadna91@gmail.com>
2024-02-03 11:39:54 +0200

    saving video data using videos.js

---
+++ data/videos.js
@@ -0,0 +1,49 @@
+const videos = [{
+    image: 'images/thumbnail-1.webp',
+    duration: '14:20',
+    channel: 'images/channel/channel-1.jpeg',
+    title: 'Talking Tech and AI with Google CEO Sundar Pichai!',
+    author: 'Marquees Brownlee',
+    views: '3.4M',
+    time: '6 months'
+}, {
+    image: 'images/thumbnail-2.webp',
+    duration: '8:22',
+    channel: 'images/channel/channel-2.jpeg',
+    title: 'Try Not To Laugh',
+    author: 'Markiplier',
+    views: '19M',
+    time: '4 months'
+}, {
+    image: 'images/thumbnail-3.webp',
+    duration: '9:13',
+    channel: 'images/channel/channel-3.jpeg',
+    title: 'Crazy Tik Toks Taken Moments Before Disaster',
+    author: 'SSSniperWolf',
+    views: '12M',
+    time: '1 year'
+}, {
+    image: 'images/thumbnail-4.webp',
+    duration: '22:09',
+    channel: 'images/channel/channel-4.jpeg',
+    title: 'The Simplest Math Problem No One can Solve - Collatz...',
+    author: 'Veritasium',
+    views: '18M',
+    time: '4 months'
+}, {
+    image: 'images/thumbnail-5.webp',
+    duration: '14:20',
+    channel: 'images/channel/channel-5.jpeg',
+    title: "Kadene's Algorithm to Maximum Subarray problem",
+    author: 'CS Dojo',
+    views: '519k',
+    time: '5 years'
+}, {
+    image: 'images/thumbnail-6.webp',
+    duration: '19:59',
+    channel: 'images/channel/channel-6.jpeg',
+    title: "Anything You Can Fit In The Circle I'll Pay For",
+    author: 'MrBeast',
+    views: '19M',
+    time: '1 year'
+}];
